1591c8d45ae4f472a08b4a3aa23d58d9416cbf6f
Leo Rondeau <christopherleorondeau@gmail.com>
2021-11-18 14:44:58 -0600

    import employee class

--- src/main/java/com/springreact/repository/EmployeeRepository.java
+++ src/main/java/com/springreact/repository/EmployeeRepository.java
@@ -1,5 +1,14 @@
 package com.springreact.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springreact.model.Employee;
+
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+	
 
 }
